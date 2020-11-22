3e14bf2e98692bd5939bb0327a315c070379e9e1
wilromeo <wilfriedkp.romeo@mail.com>
2020-11-22 20:32:35 +0100

    fixe contact mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,8 @@
+
+class ContactMailer < ApplicationMailer
+     def contact_mail(topic)
+          @topic=topic 
+          mail to:"wilfriedkp.romeo@gmailcom", subject: "vous avez publier votre topic sur forum_" 
+     end
+
+end
