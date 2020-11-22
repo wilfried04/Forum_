a7c7717dfff37cfca6a8b45ac0ffcdd82c6d51d2
baroka <birotori@gmail.com>
2020-11-22 19:17:15 +0100

    sort by catégorie, user profil, search function, modal

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,17 @@
 class FavoritesController < ApplicationController
-    def index
-        
-    end
-    def create 
-        favorite = current_user.favorites.create(topic_id: params[:topic_id]) 
-        flash[:success] = "Merci pour votre contribution à #{favorite.topic.name}" 
+    def create
+        favorite = current_user.favorites.create(topic_id: params[:topic_id])
+        flash[:success] = "Vous avez ajouté #{favorite.topic.title} à vos sujets favorit"
         redirect_to topic_path(favorite.topic.id)
-    end 
-    
-    def destroy 
-        favorite = current_user.favorites.find_by(id: params[:id]).destroy 
-        flash[:success] = "Vous n'êtes plus contributeur de #{favorite.topic.name}" 
-    end 
+    end
 
-    def show
-        @favorite = current_user.favorites.all if logged_in?
+    def destroy
+        favorite = current_user.favorites.find_by(id: params[:id]).destroy
+        flash[:success] = "#{favorite.topic.title} n'est plus dans vos favorites"
+    end
+
+    def index
+        @favorite = current_user.favorites.all
+        @topic=Topic.all
     end
 end
